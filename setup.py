1f09f7f7ee21b89feb5450d0c949d2b0bc35ad7b
Collin Barth <cpbarth92@gmail.com>
2017-10-19 00:55:16 -0700

    refactor: indentation 2: electric bugaloo

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import os
 from setuptools import (
-        find_packages,
-        setup,
-        )
+    find_packages,
+    setup,
+    )
 
 
 here = os.path.dirname(os.path.realpath(__file__))
@@ -13,21 +13,20 @@ with open(os.path.join(here, 'CHANGES.md')) as f:
 with open(os.path.join(here, 'VERSION')) as f:
     VERSION = f.read().strip()
 
-dependency_links = [
-        ]
+dependency_links = []
 install_requires = [
-        'aiohttp',
-        'pyyaml',
-        'requests',
-        'websockets',
-        ]
+    'aiohttp',
+    'pyyaml',
+    'requests',
+    'websockets',
+]
 test_requires = [
-        'pytest',
-        ]
+    'pytest',
+]
 
 data_files = [
-        ('', ['README.md', 'CHANGES.md', 'VERSION']),
-        ]
+    ('', ['README.md', 'CHANGES.md', 'VERSION']),
+]
 entry_points = {
     'console_scripts': [
         'watchdepth = scripts.watch_depth:main',
